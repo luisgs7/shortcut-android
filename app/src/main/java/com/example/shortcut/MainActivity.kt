ca6ae6099906cbfb915532d9345773851ae81835
luisgs7 <luisgsilva270@gmail.com>
2022-03-03 16:54:24 -0300

    Add function shortcut the app android.

--- app/src/main/java/com/example/shortcut/MainActivity.kt
+++ app/src/main/java/com/example/shortcut/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.shortcut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val KEY_ADD_FAVORITE = "android.intent.action.ADD_FAVORITE"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.main_text)
+
+        if(intent.action == KEY_ADD_FAVORITE){
+            text.text = "Conteúdo personalizado"
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
